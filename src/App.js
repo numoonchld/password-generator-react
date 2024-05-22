f4304828e6b5e0d3766ac2d31d3cb681a1be6620
numoonchld <raghavendra.saralaya@gmail.com>
2024-05-22 16:13:20 +0530

    baseline password generation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import passwordGenerator from "generate-password-browser";
+import { useState } from "react";
 
 function App() {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [passwordHistory, setPasswordHistory] = useState([]);
+
+  const handlePasswordGenerate = () => {
+    console.log("generate password!");
+
+    const newPassword = passwordGenerator.generate();
+
+    setCurrentPassword(newPassword);
+  };
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(currentPassword);
+    alert("copied to clipboard");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container mt-5">
+        <h1> Password Generator </h1>
+        <hr />
+        <div>
+          <h2>Generator Settings</h2>
+
+          <div className="d-flex flex-column justify-content-center align-items-center border my-5 py-5 bg-warning px-5">
+            <p className="font-monospace  fs-1 ">{currentPassword}</p>
+            <button
+              type="button"
+              className="btn btn-success w-100 mb-3"
+              onClick={handlePasswordGenerate}
+            >
+              Generate new password
+            </button>
+            <button className="btn bg-light w-100" onClick={handleCopyToClipboard}>
+              Copy to clipboard
+            </button>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <h2>Password History</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
