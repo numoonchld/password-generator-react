c13a0ecfb9983aad657854768b3af6d4ead8e27c
numoonchld <raghavendra.saralaya@gmail.com>
2024-05-22 18:01:17 +0530

    final draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import passwordGenerator from "generate-password-browser";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [currentPassword, setCurrentPassword] = useState("");
   const [passwordHistory, setPasswordHistory] = useState([]);
 
+  const [includeAlphabets, setIncludeAlphabets] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+
   const handlePasswordGenerate = () => {
-    console.log("generate password!");
+    if (
+      includeAlphabets === false &&
+      includeNumbers === false &&
+      includeSymbols === false
+    ) {
+      alert("At least one pool of elements must be active!");
+      return;
+    }
 
-    const newPassword = passwordGenerator.generate();
-
+    const newPassword = passwordGenerator.generate({
+      lowercase: includeAlphabets,
+      uppercase: includeAlphabets,
+      numbers: includeNumbers,
+      symbols: includeSymbols,
+    });
     setCurrentPassword(newPassword);
+
+    const newPasswordList = [...passwordHistory, newPassword];
+    setPasswordHistory(newPasswordList);
+    localStorage.setItem("passwordHistoryStorage", newPasswordList);
   };
 
   const handleCopyToClipboard = () => {
     navigator.clipboard.writeText(currentPassword);
-    alert("copied to clipboard");
+    alert("copied password to clipboard");
   };
+
+  useEffect(() => {
+    if (
+      includeAlphabets === false &&
+      includeNumbers === false &&
+      includeSymbols === false
+    ) {
+      alert("At least one pool of elements must be active!");
+    }
+  }, [includeAlphabets, includeNumbers, includeSymbols]);
 
   return (
     <>
@@ -24,8 +53,52 @@ function App() {
         <h1> Password Generator </h1>
         <hr />
         <div>
-          <h2>Generator Settings</h2>
-
+          <h2>Settings</h2>
+          <div className="d-flex flex-row justify-content-between mt-3">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value={includeAlphabets}
+                id="checkboxForAlphabets"
+                checked={includeAlphabets}
+                onChange={() => setIncludeAlphabets(!includeAlphabets)}
+              />
+              <label
+                className="form-check-label"
+                htmlFor="checkboxForAlphabets"
+              >
+                Alphabets
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value={includeNumbers}
+                id="checkboxForNumbers"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers(!includeNumbers)}
+              />
+              <label className="form-check-label" htmlFor="checkboxForNumbers">
+                Numbers
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value={includeSymbols}
+                id="checkboxForSymbols"
+                checked={includeSymbols}
+                onChange={() => setIncludeSymbols(!includeSymbols)}
+              />
+              <label className="form-check-label" htmlFor="checkboxForSymbols">
+                Special Characters
+              </label>
+            </div>
+          </div>
+          <hr />
           <div className="d-flex flex-column justify-content-center align-items-center border my-5 py-5 bg-warning px-5">
             <p className="font-monospace  fs-1 ">{currentPassword}</p>
             <button
@@ -35,14 +108,27 @@ function App() {
             >
               Generate new password
             </button>
-            <button className="btn bg-light w-100" onClick={handleCopyToClipboard}>
+            <button
+              className="btn bg-light w-100"
+              onClick={handleCopyToClipboard}
+            >
               Copy to clipboard
             </button>
           </div>
         </div>
         <hr />
         <div>
-          <h2>Password History</h2>
+          <h2>History</h2>
+          <ul className="list-group">
+            {passwordHistory
+              .slice(-5)
+              .reverse()
+              .map((password) => (
+                <li key={password} className="list-group-item">
+                  {password}
+                </li>
+              ))}
+          </ul>
         </div>
       </div>
     </>
